eef607ca5642296b92e04b0c9808c9d511e64af1
yahaha-wahyu <92669823+yahaha-wahyu@users.noreply.github.com>
2022-12-28 20:10:12 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Siapa Kepala Sekolah SMA Harapan Mandiri?",
+    answer: "Tse Kwok Hin",
+    options: [
+      "Laoshi Kiki",
+      "Tse Kwok Hin",
+      "Buk Diana",
+      "Pak Ridwan"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Siapa Ketua OSIS SMA Harapan Mnadiri periode 2022/2023?",
+    answer: "Yogie",
+    options: [
+      "Yogie",
+      "Samsul",
+      "Kevin",
+      "Rafi"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Siapa The Best Class SMA Harapan Mandiri Thn Ajaran 2021/2022?",
+    answer: "X IPA 2 & XI IPA 1",
+    options: [
+      "X IPA 2 & XI IPA 1",
+      "XI IPA 2 & XI IPA 1",
+      "X IPA 3 & X IPA 2",
+      "X IPA 2 & X IPA 1"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Siapa guru Sejarah SMA Harapan Mandiri?",
+    answer: "Pak Ridwan Baik",
+    options: [
+      "Pak Janter",
+      "Buk Yus",
+      "Buk Lista",
+      "Pak Ridwan Baik"
+    ]
+  },
+    {
+    numb: 5,
+    question: "1 + 99 =?",
+    answer: "100",
+    options: [
+      "seratus",
+      "100",
+      "se ratus",
+      "satu ratus"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
